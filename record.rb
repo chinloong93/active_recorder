42296f438f85d53420addc2e6e6ef68737100b7c
Goh Chin Loong <gohchinloong@gmail.com>
2016-04-11 14:43:19 -0400

    Added validation to record.rb

--- record.rb
+++ record.rb
@@ -2,16 +2,27 @@
 class Record
   # Instance variable: name of the Model using table
   attr_reader :name
-  attr_accessor :columns 
-
+  attr_accessor :columns
+  # Array with valid ActiveRecord datatypes
+  VALID_TYPES = %w(
+    binary boolean date datetime decimal float integer
+    primary_key references string text time timestamp
+    ).freeze
   # Record constructor to initialize Record with name
   def initialize(name)
+    # Validate name
+    fail ArgumentError.new('Name cannot be empty!') if name.empty?
+    fail ArgumentError.new('Name cannot be nil!') if name.nil?
+    # Initialize name and columns
     @name = name
     @columns = {}
   end
 
   # Adds a column to record
   def add_column(col, type)
+    # Check whether the column added is a valid ActiveRecord type
+    fail ArgumentError.new('Invalid ActiveRecord datatype!') unless VALID_TYPES.include?(type)
+    # Add column to record
     @columns[col] = type
   end
 end
