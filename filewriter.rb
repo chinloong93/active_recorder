795e35c7c3bf11f9f93e8b9f3a166e801cbdfd35
Goh Chin Loong <gohchinloong@gmail.com>
2016-04-17 13:48:09 -0400

    Added proper documentation (i.e. comments), cleaned up (rubocop) and tested

--- filewriter.rb
+++ filewriter.rb
@@ -3,13 +3,13 @@ class Filewriter
   # Creates a controller in the directory given
   def self.write_controller(dir, records)
     open(dir, 'w') do |f|
-      # Print class declaration
+      # Prints class declaration
       f.puts 'class TablesController < ApplicationController'
-      # Print comments
+      # Prints comment
       f.puts '  # GET /tables'
-      # Print route
+      # Prints route function
       f.puts '  def index'
-      # Print entities
+      # Prints out entities instance variable and function call
       records.each do |record|
         f.puts "    #{Filewriter.to_instance_variable(record.name)} = #{Filewriter.to_all(record.name)}"
       end
@@ -18,12 +18,12 @@ class Filewriter
     end
   end
 
-  # Creates an instance variable string out of an record name
+  # Prepends '@' and appends 's' to a lower case variable name
   def self.to_instance_variable(name)
     "@#{name.downcase}s"
   end
 
-  # Creates a string to get all entities from name
+  # Appends '.all' to a records name
   def self.to_all(name)
     "#{name}.all"
   end
