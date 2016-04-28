d318969009ff6dfc73c0b0059ef875316e915f3c
Goh Chin Loong <gohchinloong@gmail.com>
2016-04-28 17:17:19 -0400

    Updated spec_helper.rb file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_recorder'
+require 'active_recorder/record'
+require 'active_recorder/filereader'
+require 'active_recorder/filewriter'
