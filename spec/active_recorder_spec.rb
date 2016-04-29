ba1ad8bd05d5dfa2da15c1076b72cc5c87dc4bd2
Goh Chin Loong <gohchinloong@gmail.com>
2016-04-28 21:08:08 -0400

    Rubocopped code

--- spec/active_recorder_spec.rb
+++ spec/active_recorder_spec.rb
@@ -8,17 +8,17 @@ describe ActiveRecorder do
   describe '.record' do
     context 'check table views file' do
       it 'returns true if file matches' do
-        expect(FileUtils.compare_file(File.expand_path('test_files/app/views/tables/index.html.erb'),File.expand_path('test_files/actual/index.html.erb'))).to be_truthy
+        expect(FileUtils.compare_file(File.expand_path('test_files/app/views/tables/index.html.erb'), File.expand_path('test_files/actual/index.html.erb'))).to be_truthy
       end
     end
     context 'check tables controller file' do
       it 'returns true if file matches' do
-        expect(FileUtils.compare_file(File.expand_path('test_files/app/controllers/tables_controller.rb'),File.expand_path('test_files/actual/tables_controller.rb'))).to be_truthy
+        expect(FileUtils.compare_file(File.expand_path('test_files/app/controllers/tables_controller.rb'), File.expand_path('test_files/actual/tables_controller.rb'))).to be_truthy
       end
     end
     context 'check routes file' do
       it 'returns true if file matches' do
-        expect(FileUtils.compare_file(File.expand_path('test_files/config/routes.rb'),File.expand_path('test_files/actual/routes.rb'))).to be_truthy
+        expect(FileUtils.compare_file(File.expand_path('test_files/config/routes.rb'), File.expand_path('test_files/actual/routes.rb'))).to be_truthy
       end
     end
   end
